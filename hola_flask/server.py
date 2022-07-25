f36a531a9c2867ab6688affe1119382b85d8d63b
Sebastian Macias Gallego <smaciasg@unal.edu.co>
2022-07-24 22:45:12 -0500

    Envío tabla HTML

--- hola_flask/server.py
+++ hola_flask/server.py
@@ -13,6 +13,20 @@ def algosucede():
 def fase(frasex,num):
     return render_template("render.html", frasex=frasex,num=num)
 
+
+@app.route('/lists')
+def render_lists():
+    # Muy pronto, obtendremos datos de una base de datos, pero por ahora, estamos codificando datos
+    estudiantes_info = [
+        {'name' : 'Michael', 'edad' : 35},
+        {'name' : 'John', 'edad' : 30 },
+        {'name' : 'Mark', 'edad' : 25},
+        {'name' : 'KB', 'edad' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], estudiantes = estudiantes_info)
+
+
+
 @app.errorhandler(404) #Error cuando la página no existe
 def page_not_found(error):
     return 'This page does not exist', 404
